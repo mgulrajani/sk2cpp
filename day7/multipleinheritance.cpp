9cf36a62086d32c657ec4cf5a79bb028dd50186d
mgulrajani <muskaangulrajani@hotmail.com>
2024-05-08 17:17:04 +0530

    multiple inheritance

---
+++ day7/multipleinheritance.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Polygon{
+    protected:
+        int width,height;
+    public:
+        Polygon(int a,int b):width(a),height(b){}
+
+};
+class Print{
+    public:
+    static void print(int data){
+        cout<<data<<endl;
+    }
+
+};
+
+class Rectangle:public Polygon,public Print{
+    public:
+        Rectangle(int a,int b):Polygon(a,b){}
+        int calc_area(){return width*height;}
+
+
+};
+
+class Triangle:public Polygon,public Print{
+    public:
+        Triangle(int a,int b):Polygon(a,b){}
+        int calc_area(){return 0.5*width*height;}
+
+
+};
+
+int main(){
+
+Rectangle r1(4,5);
+Triangle t1(4,5);
+r1.print(r1.calc_area());
+Triangle::print(t1.calc_area());
+
+return 0;
+}
